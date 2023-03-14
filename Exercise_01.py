25fef66ed7981ba6dd5689582ba9635938b2983f
ilhamryan <milhamryank@student.uns.ac.id.com>
2023-03-14 10:24:54 +0700

    menambahkan Exercise_01

---
+++ Exercise_01.py
@@ -0,0 +1,18 @@
+print("MUHAMAD ILHAM RYAN KUSUMA")
+print("I0322080")
+
+print("="*150)
+
+print("""
+        Hallo perkenalkan nama saya Muhamad Ilham Ryan Kusuma biasa dipanggil ilham. 
+    Saya di summon ke dunia ini pada 20 Februari 2004 di Temanggung, sebuah kota kecil di Jawa Tengah.
+    Saat ini saya berkuliah di prodi Teknik Industri UNS.   
+        Saya memiliki ikatan yang kuat dengan Temanggung karena saya besar dan lahir di Temanggung.
+    Hal yang paling membuat saya cinta dengan Temanggung adalah hawa sejuknya dan pemandangan sore harinya.
+    Temanggung merupakan kota yang dikelilingi pegunungan. Di barat terdapat Gunung Sumbing dan Gunung Sindoro,
+    di utara terdapat Gunung Unggaran, dan di timur terdapat Gunung Andong, Telomoyo, Merbabu, dan Merapi. 
+    Banyaknya gunung yang mengelilingi Temanggung membuat udara di kota ini sangat sejuk dan nyaman.
+        Teknik Industri yang berusaha "make things better" yang tidak jauh dari kata efektifitas dan efisiensi 
+    membuat saya tertarik pada Teknik Industri. Saya ingin dapat membuat hal yang ada saat ini menjadi lebih baik.
+    Saya berharap melalui Teknik Industri UNS ini saya dapat meluaskan wadah ilmu pengetahuan yang saya miliki sehingga
+    saya dapat menjadi orang yang bijak dan dapat bermanfaat bagi sekitar.""")
